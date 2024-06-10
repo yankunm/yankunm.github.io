07390e171c294eca8b939ca1a2c72957fa2745ff
Yankun Meng <yankun.meng25@gmail.com>
2024-06-10 13:26:55 -0400

    updated git-workflow to ls

--- src/assets/index.js
+++ src/assets/index.js
@@ -32,6 +32,46 @@ import tripguide from "./tripguide.png";
 import workout from "./workout.png"
 import pic01 from "./pic01.png"
 
+import A0_pattern_formation_A from "./A0_pattern_formation_A.gif";
+import A0_checkpoint_pattern_formation_A from "./A0-checkpoint_pattern_formation_A_.ipynb_checkpoints.gif";
+import B0_pattern_formation_B from "./B0_pattern_formation_B.gif";
+import bulk_modulus0_acoustic_scattering_discontinuous_2d_bulk_modulus from "./bulk_modulus0_acoustic_scattering_discontinuous_2d_bulk_modulus.gif";
+import bulk_modulus0_acoustic_scattering_inclusions_2d_bulk_modulus from "./bulk_modulus0_acoustic_scattering_inclusions_2d_bulk_modulus.gif";
+import bulk_modulus0_acoustic_scattering_maze_2d_bulk_modulus from "./bulk_modulus0_acoustic_scattering_maze_2d_bulk_modulus.gif";
+import bulk_modulus0_checkpoint_acoustic_scattering_discontinuous_2d_bulk_modulus from "./bulk_modulus0-checkpoint_acoustic_scattering_discontinuous_2d_bulk_modulus_.ipynb_checkpoints.gif";
+import bulk_modulus0_checkpoint_acoustic_scattering_inclusions_2d_bulk_modulus from "./bulk_modulus0-checkpoint_acoustic_scattering_inclusions_2d_bulk_modulus_.ipynb_checkpoints.gif";
+import buoyancy0_rayleigh_benard_buoyancy from "./buoyancy0_rayleigh_benard_buoyancy.gif";
+import c_zz0_viscoelastic_instability_c_zz from "./c_zz0_viscoelastic_instability_c_zz.gif";
+import concentration0_active_matter_concentration from "./concentration0_active_matter_concentration.gif";
+import density0_acoustic_scattering_maze_2d_density from "./density0_acoustic_scattering_maze_2d_density.gif";
+import density0_euler_quadrants_density from "./density0_euler_quadrants_density.gif";
+import density0_turbulent_radiative_layer_2D_density from "./density0_turbulent_radiative_layer_2D_density.gif";
+import density0_checkpoint_acoustic_scattering_discontinuous_2d_density from "./density0-checkpoint_acoustic_scattering_discontinuous_2d_density_.ipynb_checkpoints.gif";
+import density0_checkpoint_acoustic_scattering_inclusions_2d_density from "./density0-checkpoint_acoustic_scattering_inclusions_2d_density_.ipynb_checkpoints.gif";
+import density0_checkpoint_acoustic_scattering_maze_2d_density from "./density0-checkpoint_acoustic_scattering_maze_2d_density_.ipynb_checkpoints.gif";
+import density0_checkpoint_euler_quadrants_density from "./density0-checkpoint_euler_quadrants_density_.ipynb_checkpoints.gif";
+import energy0_euler_quadrants_energy from "./energy0_euler_quadrants_energy.gif";
+import energy0_checkpoint_euler_quadrants_energy from "./energy0-checkpoint_euler_quadrants_energy_.ipynb_checkpoints.gif";
+import pressure_im0_helmholtz_staircase_pressure_im from "./pressure_im0_helmholtz_staircase_pressure_im.gif";
+import pressure_im0_checkpoint_helmholtz_staircase_pressure_im from "./pressure_im0-checkpoint_helmholtz_staircase_pressure_im_.ipynb_checkpoints.gif";
+import pressure_re0_helmholtz_staircase_pressure_re from "./pressure_re0_helmholtz_staircase_pressure_re.gif";
+import pressure_re0_checkpoint_helmholtz_staircase_pressure_re from "./pressure_re0-checkpoint_helmholtz_staircase_pressure_re_.ipynb_checkpoints.gif";
+import pressure0_acoustic_scattering_discontinuous_2d_pressure from "./pressure0_acoustic_scattering_discontinuous_2d_pressure.gif";
+import pressure0_acoustic_scattering_inclusions_2d_pressure from "./pressure0_acoustic_scattering_inclusions_2d_pressure.gif";
+import pressure0_acoustic_scattering_maze_2d_pressure from "./pressure0_acoustic_scattering_maze_2d_pressure.gif";
+import pressure0_euler_quadrants_pressure from "./pressure0_euler_quadrants_pressure.gif";
+import pressure0_rayleigh_benard_pressure from "./pressure0_rayleigh_benard_pressure.gif";
+import pressure0_shear_flow_pressure from "./pressure0_shear_flow_pressure.gif";
+import pressure0_turbulent_radiative_layer_2D_pressure from "./pressure0_turbulent_radiative_layer_2D_pressure.gif";
+import pressure0_viscoelastic_instability_pressure from "./pressure0_viscoelastic_instability_pressure.gif";
+import pressure0_checkpoint_acoustic_scattering_discontinuous_2d_pressure from "./pressure0-checkpoint_acoustic_scattering_discontinuous_2d_pressure_.ipynb_checkpoints.gif";
+import pressure0_checkpoint_acoustic_scattering_inclusions_2d_pressure from "./pressure0-checkpoint_acoustic_scattering_inclusions_2d_pressure_.ipynb_checkpoints.gif";
+import pressure0_checkpoint_acoustic_scattering_maze_2d_pressure from "./pressure0-checkpoint_acoustic_scattering_maze_2d_pressure_.ipynb_checkpoints.gif";
+import pressure0_checkpoint_euler_quadrants_pressure from "./pressure0-checkpoint_euler_quadrants_pressure_.ipynb_checkpoints.gif";
+import pressure0_checkpoint_viscoelastic_instability_pressure from "./pressure0-checkpoint_viscoelastic_instability_pressure_.ipynb_checkpoints.gif";
+import tracer0_shear_flow_tracer from "./tracer0_shear_flow_tracer.gif";
+
+
 export {
   logo,
   backend,
@@ -62,5 +102,43 @@ export {
   jobit,
   tripguide,
   workout,
-  pic01
+  pic01,
+  A0_pattern_formation_A,
+  A0_checkpoint_pattern_formation_A,
+  B0_pattern_formation_B,
+  bulk_modulus0_acoustic_scattering_discontinuous_2d_bulk_modulus,
+  bulk_modulus0_acoustic_scattering_inclusions_2d_bulk_modulus,
+  bulk_modulus0_acoustic_scattering_maze_2d_bulk_modulus,
+  bulk_modulus0_checkpoint_acoustic_scattering_discontinuous_2d_bulk_modulus,
+  bulk_modulus0_checkpoint_acoustic_scattering_inclusions_2d_bulk_modulus,
+  buoyancy0_rayleigh_benard_buoyancy,
+  c_zz0_viscoelastic_instability_c_zz,
+  concentration0_active_matter_concentration,
+  density0_acoustic_scattering_maze_2d_density,
+  density0_euler_quadrants_density,
+  density0_turbulent_radiative_layer_2D_density,
+  density0_checkpoint_acoustic_scattering_discontinuous_2d_density,
+  density0_checkpoint_acoustic_scattering_inclusions_2d_density,
+  density0_checkpoint_acoustic_scattering_maze_2d_density,
+  density0_checkpoint_euler_quadrants_density,
+  energy0_euler_quadrants_energy,
+  energy0_checkpoint_euler_quadrants_energy,
+  pressure_im0_helmholtz_staircase_pressure_im,
+  pressure_im0_checkpoint_helmholtz_staircase_pressure_im,
+  pressure_re0_helmholtz_staircase_pressure_re,
+  pressure_re0_checkpoint_helmholtz_staircase_pressure_re,
+  pressure0_acoustic_scattering_discontinuous_2d_pressure,
+  pressure0_acoustic_scattering_inclusions_2d_pressure,
+  pressure0_acoustic_scattering_maze_2d_pressure,
+  pressure0_euler_quadrants_pressure,
+  pressure0_rayleigh_benard_pressure,
+  pressure0_shear_flow_pressure,
+  pressure0_turbulent_radiative_layer_2D_pressure,
+  pressure0_viscoelastic_instability_pressure,
+  pressure0_checkpoint_acoustic_scattering_discontinuous_2d_pressure,
+  pressure0_checkpoint_acoustic_scattering_inclusions_2d_pressure,
+  pressure0_checkpoint_acoustic_scattering_maze_2d_pressure,
+  pressure0_checkpoint_euler_quadrants_pressure,
+  pressure0_checkpoint_viscoelastic_instability_pressure,
+  tracer0_shear_flow_tracer
 };
